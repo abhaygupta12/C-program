e84400e0d9145b2d1bf84a905c64dd312f84f9db
abhaygupta12 <71281005+abhaygupta12@users.noreply.github.com>
2021-10-01 00:16:47 +0530

    Add files via upload

---
+++ pattern print.cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+main()
+{
+	int i,a,j;
+	for(i=0;i<5;i++)
+	{
+		for(j=0;j<5;j++)
+		{
+			if(j<=i)
+		printf("%d",i+1);
+		else
+		printf("  ");
+        }
+    	printf("\n"); 
+		}
+     	
+     		   
+    
+}    
